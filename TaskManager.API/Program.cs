421b87c611d2c5330e0249894085996a28716778
Fuad Agazade <fuad@agazade.com>
2021-12-12 18:10:01 +0400

    Update swagger

--- TaskManager.API/Program.cs
+++ TaskManager.API/Program.cs
@@ -1,15 +1,37 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Task Manager API"
+    });
+
+    string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    string path = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+    s.IncludeXmlComments(path);
+});
 
 var app = builder.Build();
 
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(c =>
+    {
+        c.RouteTemplate = "docs/{documentname}/api.json";
+    });
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/docs/v1/api.json", $"Task Manager API V1");
+        c.RoutePrefix = "documentation";
+    });
 }
 
 app.UseHttpsRedirection();
