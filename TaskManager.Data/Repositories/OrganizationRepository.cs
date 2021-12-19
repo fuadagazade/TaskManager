65c0517d2acc10dfc0ec86d7feb10d1113eed57f
Fuad Agazade <fuad@agazade.com>
2021-12-20 03:06:31 +0400

    Added Repositories

---
+++ TaskManager.Data/Repositories/OrganizationRepository.cs
@@ -0,0 +1,89 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using TaskManager.Core.Interfaces.Repositories;
+using TaskManager.Core.Models;
+
+namespace TaskManager.Data.Repositories;
+
+public class OrganizationRepository : IOrganizationRepository
+{
+    private readonly SqlConnection context;
+
+    public OrganizationRepository()=> context = DataContext.GetConnection();
+
+    public async Task<long> Add(Organization data)
+    {
+        string command = @"INSERT INTO Organizations(Name, Tag, Phone, Address, Logo, Status) 
+                            OUTPUT INSERTED.ID 
+                            VALUES(@Name, @Tag, @Phone, @Address, @Logo, @Status)";
+
+        long result = 0;
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.ExecuteScalarAsync<long>(command, data);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result;
+    }
+
+    public Task<bool> Delete(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public long Exists(string tag)
+    {
+        string command = @"SELECT Id FROM Organizations WHERE Tag = @Tag";
+
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("@Tag", tag);
+
+        long result = 0;
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = connection.ExecuteScalar<long>(command, parameters);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result;
+    }
+
+    public Task<IEnumerable<Organization>> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Organization> Get(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Update(Organization data)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TaskManager.Data/Repositories/UserRepository.cs
@@ -0,0 +1,91 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using TaskManager.Core.Interfaces.Repositories;
+using TaskManager.Core.Models;
+
+namespace TaskManager.Data.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly SqlConnection context;
+
+    public UserRepository() => context = DataContext.GetConnection();
+
+    public async Task<long> Add(User user)
+    {
+        string command = @"INSERT INTO Users(FirstName, LastName, Email, Password, Image, Role, OrganizationId, Approved, Status) 
+                            OUTPUT INSERTED.ID 
+                            VALUES(@FirstName, @LastName, @Email, @Password, @Image, @Role, @OrganizationId, @Approved, @Status)";
+
+        long result = 0;
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.ExecuteScalarAsync<long>(command, user);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result;
+    }
+
+    public Task<bool> Delete(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public long Exists(string email, string tag)
+    {
+        string command = @"SELECT U.Id FROM Users AS U INNER JOIN Organizations AS O ON U.OrganizationId = O.Id WHERE U.Email = @Email AND O.Tag = @Tag";
+
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("@Email", email);
+        parameters.Add("@Tag", tag);
+
+
+        long result = 0;
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = connection.ExecuteScalar<long>(command, parameters);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result;
+    }
+
+    public Task<IEnumerable<User>> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> Get(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Update(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
