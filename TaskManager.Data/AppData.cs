be4eed37b3df36936eccaa400805df3519ed820f
Fuad Agazade <fuad@agazade.com>
2021-12-20 03:06:47 +0400

    Added AppData

---
+++ TaskManager.Data/AppData.cs
@@ -0,0 +1,20 @@
+﻿using TaskManager.Core.Interfaces.Repositories;
+using TaskManager.Data.Repositories;
+
+
+namespace TaskManager.Data;
+
+public class AppData : IAppData
+{
+    private IOrganizationRepository organizations;
+    private IUserRepository users;
+
+
+    public IOrganizationRepository Organizations => organizations ?? new OrganizationRepository();
+    public IUserRepository Users => users ?? new UserRepository();
+
+    public AppData()
+    {
+
+    }
+}
