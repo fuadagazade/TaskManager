d3ecf2f78c7e32f2f0c5c30b28596840741888cf
Fuad Agazade <fuad@agazade.com>
2021-12-20 03:09:28 +0400

    Updated OrganizationValidator

--- TaskManager.Core/Validators/OrganizationValidator.cs
+++ TaskManager.Core/Validators/OrganizationValidator.cs
@@ -1,19 +1,20 @@
 ﻿using FluentValidation;
+using TaskManager.Core.Interfaces.Services;
 using TaskManager.Core.Models;
 
 namespace TaskManager.Core.Validators;
 public class OrganizationValidator : AbstractValidator<Organization>
 {
-    public OrganizationValidator()
+    public OrganizationValidator(IOrganizationService organizationService)
     {
-        RuleFor(x => x.Name)
-            .NotNull().WithMessage("Name is null")
-            .NotEmpty().WithMessage("Name is empty")
-            .Length(1, 150).WithMessage("Minimum lenght of Name is 1, Maximum is 150");
+        RuleFor(x => x.Name).Length(1, 150);
 
-        RuleFor(x=>x.Tag)
-            .NotNull().WithMessage("Tag is null")
-            .NotEmpty().WithMessage("Tag is empty")
-            .Length(1, 150).WithMessage("Minimum lenght of Tag is 1, Maximum is 150");
+        RuleFor(x=>x.Tag).Length(1, 150).Custom((x, context) =>
+        {
+            if (organizationService.Exists(x) > 0)
+            {
+                context.AddFailure("Organization with this tag name exists");
+            }
+        }); ;
     }
 }
