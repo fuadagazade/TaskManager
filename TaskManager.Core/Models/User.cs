847b82cb44d0612a07d4fb70d0b392924267a619
Fuad Agazade <fuad@agazade.com>
2021-12-20 03:04:04 +0400

    Added User

---
+++ TaskManager.Core/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+using TaskManager.Core.Enumerations;
+
+namespace TaskManager.Core.Models;
+public class User : BaseModel
+{
+    [JsonPropertyName("firstName")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("lastName")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+
+    [JsonPropertyName("image")]
+    public string Image { get; set; }
+
+    [JsonPropertyName("role")]
+    public Role Role { get; set; }
+
+    [JsonPropertyName("approved")]
+    public bool Approved { get; set; }
+
+    [JsonPropertyName("organizationId")]
+    public long OrganizationId { get; set; }
+}
