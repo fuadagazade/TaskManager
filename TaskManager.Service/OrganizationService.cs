57c6012f04363f23e6c9e6c63279a99f89c304a9
Fuad Agazade <fuad@agazade.com>
2021-12-20 03:07:04 +0400

    Added Services

---
+++ TaskManager.Service/OrganizationService.cs
@@ -0,0 +1,25 @@
+﻿using TaskManager.Core.Interfaces.Repositories;
+using TaskManager.Core.Interfaces.Services;
+using TaskManager.Core.Models;
+
+namespace TaskManager.Service;
+
+public class OrganizationService : IOrganizationService
+{
+    private readonly IAppData _appData;
+    public OrganizationService(IAppData appData) => _appData = appData;
+
+    public async Task<long> Add(Organization organization)
+    {
+        long organizationId = await this._appData.Organizations.Add(organization);
+
+        return organizationId;
+    }
+
+    public long Exists(string tag)
+    {
+        long organizationId = this._appData.Organizations.Exists(tag);
+
+        return organizationId;
+    }
+}
---
+++ TaskManager.Service/UserService.cs
@@ -0,0 +1,25 @@
+﻿using TaskManager.Core.Interfaces.Repositories;
+using TaskManager.Core.Interfaces.Services;
+using TaskManager.Core.Models;
+
+namespace TaskManager.Service;
+
+public class UserService : IUserService
+{
+    private readonly IAppData _appData;
+    public UserService(IAppData appData) => _appData = appData;
+
+    public async Task<long> Add(User user)
+    {
+        long userId = await this._appData.Users.Add(user);
+
+        return userId;
+    }
+
+    public long Exists(string email, string tag)
+    {
+        long userId = this._appData.Users.Exists(email, tag);
+
+        return userId;
+    }
+}
