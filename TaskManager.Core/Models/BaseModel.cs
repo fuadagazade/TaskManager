0e699697699af682ba367a3abde6bc82b60bb637
Fuad Agazade <fuad@agazade.com>
2021-12-20 03:03:36 +0400

    Added BaseModel

---
+++ TaskManager.Core/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using TaskManager.Core.Enumerations;
+
+namespace TaskManager.Core.Models;
+public class BaseModel
+{
+    [JsonPropertyName("id")]
+    [Key]
+    public long? Id { get; set; }
+
+    [JsonPropertyName("status")]
+    public Status Status { get; set; }
+}
