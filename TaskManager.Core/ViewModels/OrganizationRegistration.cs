b7fe03b591f4665c2e4c0f473092ee13882cbc79
Fuad Agazade <fuad@agazade.com>
2021-12-20 03:04:33 +0400

    Added ViewModels

---
+++ TaskManager.Core/ViewModels/OrganizationRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TaskManager.Core.ViewModels;
+public class OrganizationRegistration
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("tag")]
+    public string Tag { get; set; }
+
+    [JsonPropertyName("phone")]
+    public string Phone { get; set; }
+
+    [JsonPropertyName("address")]
+    public string Address { get; set; }
+
+    [JsonPropertyName("firstName")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("lastName")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+}
