bf4631a5de1629b490dc233fa807ef4835e144cf
Fuad Agazade <fuad@agazade.com>
2021-12-20 03:03:48 +0400

    Added Organization

---
+++ TaskManager.Core/Models/Organization.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TaskManager.Core.Models;
+public class Organization : BaseModel
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("tag")]
+    public string Tag { get; set; }
+
+    [JsonPropertyName("phone")]
+    public string Phone { get; set; }
+
+    [JsonPropertyName("address")]
+    public string Address { get; set; }
+
+    [JsonPropertyName("logo")]
+    public string Logo { get; set; }
+}
