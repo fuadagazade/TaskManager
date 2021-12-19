b01c541ba6944459a7c79fd65d0f2807b8769ee6
Fuad Agazade <fuad@agazade.com>
2021-12-20 03:06:16 +0400

    Added DataContext

---
+++ TaskManager.Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Azersun.Audit.Utilities;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace TaskManager.Data;
+
+public static class DataContext
+{
+    private static IConfigurationSection ConnectionStrings = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("ConnectionStrings");
+
+    public static SqlConnection GetConnection()
+    {
+        string ConnectionString = ConnectionStrings["Main"];
+
+        string connection = Cryptographer.Decrypt(ConnectionString);
+
+        return new SqlConnection(connection);
+    }
+
+    public static void GenerateDatabase()
+    {
+        using (SqlConnection connection = GetConnection())
+        {
+            DatabaseGenerator.Install(connection);
+        }
+    }
+}
