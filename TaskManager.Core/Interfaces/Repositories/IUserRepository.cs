112937178324c34b401f664b5346b13f37536b9e
Fuad Agazade <fuad@agazade.com>
2021-12-20 11:11:53 +0400

    Updated User Repository

--- TaskManager.Core/Interfaces/Repositories/IUserRepository.cs
+++ TaskManager.Core/Interfaces/Repositories/IUserRepository.cs
@@ -1,9 +1,15 @@
-﻿using TaskManager.Core.Models;
+﻿using TaskManager.Core.Enumerations;
+using TaskManager.Core.Models;
 
 namespace TaskManager.Core.Interfaces.Repositories;
 
 public interface IUserRepository : IBaseRepository<User>
 {
-    long Exists(string email, string tag);
-    long Exists(string email, long organizationId);
+    long Exists(string email, string tag, long userId = 0);
+    long Exists(string email, long organizationId, long userId = 0);
+    Task<bool> DeleteByOrganization(long id);
+    Task<IEnumerable<User>> GetByOrganization(long id);
+    Task<bool> UpdatePassword(long userId, string password);
+    Task<bool> UpdateRole(long userId, Role role);
+
 }
--- TaskManager.Data/Repositories/UserRepository.cs
+++ TaskManager.Data/Repositories/UserRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using System.Data;
 using System.Data.SqlClient;
+using TaskManager.Core.Enumerations;
 using TaskManager.Core.Interfaces.Repositories;
 using TaskManager.Core.Models;
 
@@ -39,19 +40,70 @@ public class UserRepository : IUserRepository
         return result;
     }
 
-    public Task<bool> Delete(long id)
+    public async Task<bool> Delete(long id)
     {
-        throw new NotImplementedException();
+        string command = @"UPDATE Users SET STATUS = -1 WHERE Id = @Id";
+
+        int result = 0;
+
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("@Id", id);
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.ExecuteAsync(command, parameters);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result > 0;
     }
 
-    public long Exists(string email, string tag)
+    public async Task<bool> DeleteByOrganization(long id)
     {
-        string command = @"SELECT U.Id FROM Users AS U INNER JOIN Organizations AS O ON U.OrganizationId = O.Id WHERE U.Email = @Email AND O.Tag = @Tag";
+        string command = @"UPDATE Users SET STATUS = -1 WHERE Organization = @Id";
+
+        int result = 0;
+
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("@Id", id);
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.ExecuteAsync(command, parameters);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result > 0;
+    }
+
+    public long Exists(string email, string tag, long userId = 0)
+    {
+        string command = @"SELECT U.Id FROM Users AS U INNER JOIN Organizations AS O ON U.OrganizationId = O.Id WHERE U.Email = @Email AND O.Tag = @Tag AND U.Id <> @UserId";
 
         DynamicParameters parameters = new DynamicParameters();
         parameters.Add("@Email", email);
         parameters.Add("@Tag", tag);
-
+        parameters.Add("@UserId", userId);
 
         long result = 0;
 
@@ -74,14 +126,14 @@ public class UserRepository : IUserRepository
         return result;
     }
 
-    public long Exists(string email, long organizationId)
+    public long Exists(string email, long organizationId, long userId = 0)
     {
-        string command = @"SELECT Id FROM Users WHERE Email = @Email AND OrganizationId = @OrganizationId";
+        string command = @"SELECT Id FROM Users WHERE Email = @Email AND OrganizationId = @OrganizationId AND Id <> @UserId";
 
         DynamicParameters parameters = new DynamicParameters();
         parameters.Add("@Email", email);
         parameters.Add("@OrganizationId", organizationId);
-
+        parameters.Add("@UserId", userId);
 
         long result = 0;
 
@@ -104,18 +156,167 @@ public class UserRepository : IUserRepository
         return result;
     }
 
-    public Task<IEnumerable<User>> Get()
+    public async Task<IEnumerable<User>> Get()
     {
-        throw new NotImplementedException();
+        string command = @"SELECT * FROM Users WHERE Status > 0";
+
+        IEnumerable<User> result = null;
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.QueryAsync<User>(command);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result;
     }
 
-    public Task<User> Get(long id)
+    public async Task<IEnumerable<User>> GetByOrganization(long id)
     {
-        throw new NotImplementedException();
+        string command = @"SELECT * FROM Users WHERE Status > 0 AND OrganizationId = @Id";
+
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("@Id", id);
+
+        IEnumerable<User> result = null;
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.QueryAsync<User>(command, parameters);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result;
     }
 
-    public Task<bool> Update(User user)
+    public async Task<User> Get(long id)
     {
-        throw new NotImplementedException();
+        string command = @"SELECT * FROM Users WHERE Status > 0 AND Id = @Id";
+
+        User result = null;
+
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("@Id", id);
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.QueryFirstOrDefaultAsync<User>(command, parameters);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result;
+    }
+
+    public async Task<bool> Update(User user)
+    {
+        string command = @"UPDATE Users SET FirstName = @FirstName, LastName = @LastName, Email = @Email, Password = @Password, Image = @Image, Role = @Role, OrganizationId = @OrganizationId, Approved = @Approved, Status = @Status WHERE Id = @Id";
+
+        int result = 0;
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.ExecuteAsync(command, user);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result > 0;
+    }
+
+    public async Task<bool> UpdatePassword(long userId, string password)
+    {
+        string command = @"UPDATE Users SET Password = @Password WHERE Id = @Id";
+
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("@Password", password);
+        parameters.Add("@Id", userId);
+
+        int result = 0;
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.ExecuteAsync(command, parameters);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result > 0;
+    }
+
+    public async Task<bool> UpdateRole(long userId, Role role)
+    {
+        string command = @"UPDATE Users SET Role = @Role WHERE Id = @Id";
+
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("@Role", role);
+        parameters.Add("@Id", userId);
+
+        int result = 0;
+
+        using (IDbConnection connection = context)
+        {
+            try
+            {
+                result = await connection.ExecuteAsync(command, parameters);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Dispose();
+            }
+        }
+
+        return result > 0;
     }
 }
