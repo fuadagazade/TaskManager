94ebc5087e9697aa347909793bef3e3cb04ca95b
Fuad Agazade <fuad@agazade.com>
2021-12-20 23:50:46 +0400

    Added Authentication Model And Validation

---
+++ TaskManager.Core/Models/Authentication.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TaskManager.Core.Models;
+public class Authentication
+{
+    [JsonPropertyName("organization")]
+    public string Organization { get; set; }
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+
+    [JsonPropertyName("password")]
+    public string Password { get; set; }
+}
---
+++ TaskManager.Core/Validators/AuthenticationValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using TaskManager.Core.Interfaces.Services;
+using TaskManager.Core.Models;
+
+namespace TaskManager.Core.Validators;
+public class AuthenticationValidator : AbstractValidator<Authentication>
+{
+    public AuthenticationValidator(IOrganizationService organizationService)
+    {
+        RuleFor(x => x.Organization).Length(1, 150).Custom((x, context) =>
+        {
+            if (organizationService.Exists(x) == 0)
+            {
+                context.AddFailure("Organization with this tag name not exists");
+            }
+        });
+        RuleFor(x => x.Email).Length(1, 200).EmailAddress(EmailValidationMode.AspNetCoreCompatible);
+
+    }
+}
